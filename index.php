4563f74a0b022925b5428f5726ce02699e0f39e9
simplenick2010 <simplenick2010@gmail.com>
2016-12-17 05:19:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+	<head>
+		<title>
+			Portfolio
+		</title>
+		 <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!--https://www.youtube.com/watch?v=p4hU45Le9sY-->
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<!--iCon CDN-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="mycss/myscss.css">
+	</head>
+	<body>
+		<form>
+			<div class="container-fluid top_bar" style="background-color:#628cd1;padding:3px;">
+					<div class="container">
+						<div class="row">	
+						
+							<div class="col-sm-3" style="border:0px solid;">
+												<a href=""><i class="fa fa-facebook" aria-hidden="true" style="color:white;"></i></a>
+												<a href=""><i class="fa fa-twitter" aria-hidden="true" style="color:white;"></i></a>
+												<a href=""><i class="fa fa-linkedin" aria-hidden="true" style="color:white;"></i></a>
+												<a href=""><i class="fa fa-youtube" aria-hidden="true"style="color:white;"></i></i></a>
+												<a href=""><i class="fa fa-google-plus-square" aria-hidden="true" style="color:white;"></i></a>
+										
+							</div>	
+							<div class="col-sm-9" style="text-align:right;border:0px solid;">
+								<i class="fa fa-envelope" aria-hidden="true"></i>simplenick2010@gmail.com, 
+								<i class="fa fa-phone" aria-hidden="true"></i> +639339294352
+							</div>					
+						</div>
+					</div><!--End of container--->
+			</div><!--End of container fluid-->
+			<div class="container">	
+				<div class="row">
+			
+					<div class ="col-sm-5" style="border:0px solid;">
+					<a href=""><img src="images/memyself.png"></img></a>
+					</div><!--End of col5-->
+					
+					<div class ="col-sm-7 my_menu">
+						<nav class="navbar navbar-default">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div><!--End of header--->
+						
+							<div class="collapse navbar-collapse" id="mynavbar">
+								<ul class="nav navbar-nav pull-right">
+									<li><a href="">Dashboard</a></li>
+									<li><a href="">Profile</a></li>
+									<li><a href="">Blog</a></li>
+									<li><a href="">Portfolio</a></li>
+									<li><a href="">Resume</a></li>
+									<li><a href="">Contact</a></li>
+								</ul>
+							</div>		
+						</nav>							
+					</div> <!--End of col7--->
+				</div><!---End of row-->
+			</div> <!--End of container-->
+			
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+					</div>
+					<div class="col-sm-4">
+					</div>
+					<div class="col-sm-4">
+					</div>		
+				</div>
+			</div>
+			 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+			 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+       
+		</form>
+	</body>
+</html>
